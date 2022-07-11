2551429cacbd7422d87265fa84b7f90d31d3eabb
Timoha <66816816+TimohaGG@users.noreply.github.com>
2022-07-12 00:05:46 +0200

    Add files via upload
    Adding library DraughtsComponents for dev branch

---
+++ DraughtsComponents/Draught.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraughtsComponents
+{
+    public partial class Player
+    {
+        public partial class Draught
+        {
+            //----------fields\props---------
+            internal char skin;
+            internal int x;
+            internal int y;
+            public bool isChoosen { get; internal set; }
+
+            //----------constructor----------
+            public Draught(int x, int y, char skin)
+            {
+                this.x = x;
+                this.y = y;
+                this.skin = skin;
+                isChoosen = false;
+            }
+            //---------enums----------
+            public enum _direction
+            {
+                UP_R,
+                UP_L,
+                DOWN_R,
+                DOWN_L
+            }
+
+            //----------methods----------
+            public void Use(_direction direction)
+            {
+                switch (direction)
+                {
+                    case _direction.UP_R:
+                        y--;
+                        x++;
+                        break;
+                    case _direction.UP_L:
+                        y--;
+                        x--;
+                        break;
+                    case _direction.DOWN_R:
+                        y++;
+                        x++;
+                        break;
+                    case _direction.DOWN_L:
+                        y++;
+                        x--;
+                        break;
+                }
+            }
+           
+            
+        }
+    }
+}
---
+++ DraughtsComponents/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraughtsComponents
+{
+    public partial class Player
+    {
+        //---------inserted class---------
+        public partial class Draught{}
+
+        //----------fields/props----------
+        private const int draughtsAmount=12;
+        public Draught[] arr=new Draught[draughtsAmount];
+        internal string name;
+
+        //----------constructor----------
+        public Player(string name, char skin, int xFirst, int yFirst) {
+            for (int i = 0; i < draughtsAmount; i++)
+            {
+                arr[i] = new Draught(0,0,skin);
+                //paste code here
+            }
+        }
+        //---------methods----------
+        public bool isKill(Draught[] arrEnemy)
+        {
+            for (int i = 0; i < draughtsAmount; i++)
+            {
+                foreach (var item in arrEnemy)
+                {
+                    if (item.x == arr[i].x && item.y == arr[i].y)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
