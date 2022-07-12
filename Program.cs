2b88701a19c08b3a3746fd5ca4f4b934c83d143f
SulX_lj39 <SulX_lj39@student.itstep.org>
2022-07-12 20:11:43 +0200

    Finishing choosing movement phisics

--- Program.cs
+++ Program.cs
@@ -8,31 +8,44 @@ namespace Draughts_v2
 {
     internal class Program
     {
+        
+        static bool PlayerTurn(Player player, Player player2, Field field)
+        {
+            ConsoleKey key;
+            field.PrintField(player.arr[player.GetChoosenDraughtIndex()]);
+            key = Console.ReadKey().Key;
+            if (key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                return false;
+            }
+            else
+            {
+
+                player.MoveSelection(key);
+                Console.SetCursorPosition(0, 0);
+                return true;
+            }
+        }
         static void Main(string[] args)
         {
             
             Player player = new Player("Tim", 'w', 1, 0);
-            //player.PrintCoords();
-            //Console.WriteLine();
+           
             Player player2 = new Player("Makar", 'b', 0, 5);
-            //player2.PrintCoords();
+           
             Field field = new Field(player.arr, player2.arr);
-            ConsoleKey key;
+           
             while (true)
             {
-                field.PrintField(player.arr[player.GetChoosenDraughtIndex()]);
-                 key= Console.ReadKey().Key;
-                if(key == ConsoleKey.Enter)
+                while(PlayerTurn(player,player2, field))
+                {
+                    
+                }
+                while (PlayerTurn(player2, player, field))
                 {
 
                 }
-                else
-                {
-                    
-                    player.MoveSelection(key);
-                    Console.SetCursorPosition(0,0);
-                }
-                
             }
             
            
