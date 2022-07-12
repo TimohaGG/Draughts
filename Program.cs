c29cf438dcd2d08a1bdf8ce432dd4d74bb13852e
SulX_lj39 <SulX_lj39@student.itstep.org>
2022-07-12 11:07:41 +0200

    Adding field

--- Program.cs
+++ Program.cs
@@ -10,11 +10,25 @@ namespace Draughts_v2
     {
         static void Main(string[] args)
         {
-
-            Player player = new Player("Tim",'w', 1,1);
+            ConsoleKey consoleKey = (ConsoleKey.LeftArrow);
+            int code = (int)consoleKey;
+            Console.WriteLine(code);
+            Console.ReadLine();
+            Player player = new Player("Tim", 'w', 1, 0);
+            //player.PrintCoords();
+            //Console.WriteLine();
+            Player player2 = new Player("Makar", 'b', 0, 5);
+            //player2.PrintCoords();
+            Field field = new Field(player.arr, player2.arr);
+            while (true)
+            {
+                field.PrintField();
+                ConsoleKey key = Console.ReadKey().Key;
+                
+            }
             
-            
-
+           
+            Console.Read();
         }
     }
 }
